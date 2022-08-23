6b9e2d4691808a8f9b6d91d89dd5368ae0ff1e0c
timursk <timur.dev@mail.ru>
2022-08-24 00:56:25 +0300

    feat: create Card component, install styled-components, upd next config

---
+++ my-app/components/Card/Card.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const Card = ({name, background_image, rating, released}) => {
+  return (
+    <Container>
+      <Image src={background_image} alt={name} />
+      <div>
+        <h3>{name}</h3>
+        <p>{rating}</p>
+        <p>{released}</p>
+      </div>
+    </Container>
+  )
+}
+const Container = styled.div`
+  // width: 200px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid black;
+  border-radius: 1rem;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+`;
+
+const Image = styled.img`
+  width: 200px;
+  color: black;
+`;
+
